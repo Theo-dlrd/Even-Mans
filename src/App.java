ee32b7e0484ec24c2747dad5ee81f4da2a5525b2
Theo-dlrd <Theo.Duluard.Etu@univ-lemans.fr>
2023-08-12 23:49:51 +0200

    test

--- src/App.java
+++ src/App.java
@@ -1,27 +1,40 @@
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
 public class App extends Application {
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
-		primaryStage.initStyle(StageStyle.DECORATED);
-		
-		Scene sc = new Scene(new Group(), 1280, 720);
-		primaryStage.setScene(sc);
-		
-		primaryStage.setTitle("Even'Mans");
-		
-		primaryStage.show();
-	}
-
-	public static void main(String[] args) 
-	{
-		launch(args);
-	}
-
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+  
+  /*
+  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+  Scene scene = new Scene(root);
+  */
+  
+  Scene scene = new Scene(root, 300, 250);
+  
+  primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
+    }
 }
